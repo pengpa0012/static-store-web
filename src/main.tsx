f81e378d39363470eb5a02e09b0113e142c430b0
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-11 15:51:45 +0800

    setup routes. tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import About from './pages/About.tsx'
+import Checkout from './pages/Checkout.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
