c77e28495d67928d8ead8e2c72ad527f9d04c744
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-16 08:46:58 +0800

    update slider, text, added util

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from 'react'
-import { close1, close6, close7, flat, icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, phone1, phone10, phone15, phone17, phone18, phone2, phone3, slide1, slide2, slide3, slide4, slide5, test2 } from "./assets/index"
+import { close1, close6, close7, flat, phone1, phone15, phone16, phone17, phone18, phone2, phone3, slide1, slide2, slide3, slide4, slide5, test2 } from "./assets/index"
 import './App.css'
 import React from 'react'
+import { featContent } from './util/content'
 
 function App() {
   const productRef = useRef<HTMLImageElement>(null)
@@ -44,9 +45,9 @@ function App() {
           <img src={phone15} className="w-full max-w-[600px] ml-24" />
         </div>
         {
-          [1,2,3].map(el => (
+          ["Good for filming","Photography","and Gaming..."].map(el => (
             <div className='min-h-screen flex justify-end' key={el}>
-              <p className="text-5xl font-bold leading-snug w-1/2">Lorem ipsum dolor sit</p>
+              <p className="text-7xl font-bold leading-snug w-1/2">{el}</p>
             </div>
           ))
         }
@@ -63,12 +64,14 @@ function App() {
         <h2 className="text-7xl font-bold text-center">Capture the moments...</h2>
       </div>
       <div className='min-h-screen flex flex-col items-center justify-center'>
-        <img src={phone18} className="w-full max-w-[1000px] backdrop-hue-rotate-180 z-10 pointer-events-none px-4" />
-        <div className="slider py-12 flex gap-5 -translate-y-[90%] overflow-x-scroll">
+        <div className='image-slider'>
+          <img src={phone18} className="w-full max-w-[1000px] z-10 pointer-events-none" />
+        </div>
+        <div className="slider py-12 flex gap-5 -translate-y-[92%]">
           {
             [slide1,slide2,slide3,slide4,slide5,slide1,slide2,slide3,slide4,slide5].map((el, i) => (
               <React.Fragment key={i}>
-                <img src={el} className="w-full max-w-[1000px] h-[477px] hue-rotate-180" />
+                <img src={el} className="w-full max-w-[1000px] h-[477px] invert" />
               </React.Fragment>
             ))
           }
@@ -81,55 +84,37 @@ function App() {
         <div className="flex items-center gap-10">
           <div className='flex flex-1 flex-col gap-5'>
             <div className="flex gap-5">
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon1} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon2} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon3} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
+              {
+                featContent[0].map(el => (
+                  <div className="flex flex-1 gap-2 items-center rounded-md p-2">
+                    <img src={el.icon} className='w-[50px] h-[50px]' />
+                    <p className="text-xs
+                    ">{el.text}</p>
+                  </div>
+                ))
+              }
             </div>
             <div className="flex gap-5">
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon4} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon5} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon6} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
+              {
+                featContent[1].map(el => (
+                  <div className="flex flex-1 gap-2 items-center rounded-md p-2">
+                    <img src={el.icon} className='w-[50px] h-[50px]' />
+                    <p className="text-xs
+                    ">{el.text}</p>
+                  </div>
+                ))
+              }
             </div>
             <div className="flex gap-5">
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon7} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon8} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
-              <div className="flex gap-2 border border-black rounded-md p-2">
-                <img src={icon9} className='w-[50px] h-[50px]' />
-                <p className="text-xs
-                ">Some text</p>
-              </div>
+              {
+                featContent[2].map(el => (
+                  <div className="flex flex-1 gap-2 items-center rounded-md p-2">
+                    <img src={el.icon} className='w-[50px] h-[50px]' />
+                    <p className="text-xs
+                    ">{el.text}</p>
+                  </div>
+                ))
+              }
             </div>
           </div>
           <img src={phone17} className='flex-1 h-[450px] object-cover'/>
@@ -137,13 +122,13 @@ function App() {
       </div>
       <div className='min-h-screen max-w-[1440px] mx-auto px-4'>
         <h4 className="text-5xl font-bold mb-4">Heading.<br /> Lorem ipsum dolor sit.</h4>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center gap-1">
           <img src={close1} className='w-full max-h-[600px] aspect-square object-cover'/>
-          <div className="flex">
+          <div className="flex gap-1">
             <img src={close6} className='w-1/2 max-h-[500px] flex-1/2 aspect-square object-cover'/>
             <img src={close7} className='w-1/2 max-h-[500px] flex-1/2 aspect-square object-cover'/>
           </div>
-          <div className="flex">
+          <div className="flex gap-1">
             <img src={phone1} className='w-4/12 max-h-[400px] aspect-square object-cover'/>
             <img src={phone2} className='w-4/12 max-h-[400px] aspect-square object-cover'/>
             <img src={phone3} className='w-4/12 max-h-[400px] aspect-square object-cover'/>
@@ -153,9 +138,9 @@ function App() {
       <div className="min-h-screen grid place-items-center">
         <div className="flex gap-8">
           {
-            [1,2,3].map(el => (
-              <div className='text-center' key={el}>
-                <img src={phone10} className="w-[250px] mb-4" />
+            [phone16,phone16,phone16].map((el, i) => (
+              <div className='text-center' key={i}>
+                <img src={el} className="w-[250px] object-cover mb-4" />
                 <h4 className='font-bold text-2xl'>Product name</h4>
                 <p>Awesome</p>
                 <p>₱56,990</p>
---
+++ src/util/content.ts
@@ -0,0 +1,18 @@
+import { icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon9 } from "../assets";
+
+export const featContent = [
+  [ 
+    {icon: icon1, text:"Battery"},
+    {icon: icon2, text:"Camera"},
+    {icon: icon3, text:"Charger"}
+  ],
+  [
+    {icon: icon4, text:"CPU"},
+    {icon: icon5, text:"RAM"},
+    {icon: icon6, text:"ROM"}
+  ],
+  [ {icon: icon7, text:"Video"},
+    {icon: icon7, text:"Resolution"},
+    {icon: icon9, text:"Fingerprint"},
+  ]
+]
