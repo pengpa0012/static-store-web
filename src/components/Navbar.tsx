9fb9117a1341596b0a92723950b0ff2ce40fcc50
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-18 11:54:13 +0800

    added scroll top on click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom"
 
 function Navbar() {
+
+  function scrollTop() {
+    window.scrollTo({top: 0})
+  }
   return (
     <div className="bg-black/90 sticky top-0 h-[50px] -mb-[50px] z-20">
       <div className="max-w-[1440px] mx-auto flex items-center justify-between text-white py-3 px-4 h-full">
-        <h1>
+        <h1 onClick={scrollTop}>
           <Link to="/">Logo</Link>
         </h1>
         <ul className="flex gap-10">
-          <li>
+          <li onClick={scrollTop}>
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li onClick={scrollTop}>
             <Link to="/products">Products</Link>
           </li>
-          <li>
+          <li onClick={scrollTop}>
             <Link to="/cart">Cart</Link>
           </li>
         </ul>
